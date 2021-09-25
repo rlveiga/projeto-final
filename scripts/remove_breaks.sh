0608308ee1998c8764e5b292ef0162cedcb20409
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2021-09-25 19:04:32 -0300

    Updating remove_breaks script

--- scripts/remove_breaks.sh
+++ scripts/remove_breaks.sh
@@ -1,9 +1,9 @@
 for f in corpus/whitepapers/*.txt
 do
-  tr '\n' ' ' < "$f" > whitepaper_$(basename "$f" .txt).txt
+  tr '\n' ' ' < "$f" > $(basename "$f" .txt).txt
 done
 
 for f in corpus/academic/*.txt
 do
-  tr '\n' ' ' < "$f" > academic_$(basename "$f" .txt).txt
+  tr '\n' ' ' < "$f" > $(basename "$f" .txt).txt
 done
