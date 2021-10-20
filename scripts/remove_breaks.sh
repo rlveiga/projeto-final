d476428f47011383f866b69fd4e0439086087147
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2021-10-19 21:17:23 -0300

    Updating remove_breaks script

--- scripts/remove_breaks.sh
+++ scripts/remove_breaks.sh
@@ -1,9 +1,9 @@
 for f in corpus/whitepapers/*.txt
 do
-  tr '\n' ' ' < "$f" > $(basename "$f" .txt).txt
+  tr '\n' ' ' < "$f" > dygiepp/data/blockchain_whitepapers/raw_data/$(basename "$f" .txt).txt
 done
 
 for f in corpus/academic/*.txt
 do
-  tr '\n' ' ' < "$f" > $(basename "$f" .txt).txt
+  tr '\n' ' ' < "$f" > dygiepp/data/blockchain_academia/raw_data/$(basename "$f" .txt).txt
 done
